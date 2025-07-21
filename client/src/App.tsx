c87e2f978a895baf4b8fd4a121c919e1e5a2d25d
Liam Von Astoria <hello@liamvonastoria.net>
2025-07-21 20:09:29 +0200

    init commit

---
+++ client/src/App.tsx
@@ -0,0 +1,49 @@
+/**
+ * ==============================================================
+ * Project     : Zenth Cloud – Zenth Panel
+ * File        : App.tsx
+ * Version     : 1.0.0
+ * Description : Client Application Component
+ * Author      : Sky Genesis Enterprise
+ * Created on  : 2025-07-19
+ * License     : AGPLv3
+ * Forked from : N/A
+ * Modified by : Liam Dispa <liam.dispa@skygenesisenterprise.com> (2025-07-20)
+ * ==============================================================
+ */
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './styles/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/main.tsx
@@ -0,0 +1,24 @@
+/**
+ * ==============================================================
+ * Project     : Zenth Cloud – Zenth Panel
+ * File        : main.tsx
+ * Version     : 1.0.0
+ * Description : Client Entry Point
+ * Author      : Sky Genesis Enterprise
+ * Created on  : 2025-07-19
+ * License     : AGPLv3
+ * Forked from : N/A
+ * Modified by : Liam Dispa <liam.dispa@skygenesisenterprise.com> (2025-07-20)
+ * ==============================================================
+ */
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
